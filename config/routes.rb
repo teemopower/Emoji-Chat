5913529b1c2a67e17605d535c0674843132f89fa
zaturnvalley <zacharycmason@gmail.com>
2016-09-08 14:31:58 -0700

    updated proto front page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  root 'rooms#show'
+  root 'home#index'
+
+  get 'rooms/show'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
