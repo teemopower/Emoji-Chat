c4ee985f759265386be5c172500cddedd364664d
zaturnvalley <zacharycmason@gmail.com>
2016-09-08 16:26:14 -0700

    created user db. about to uninstall sqlite and install postgresql

---
+++ app/models/user.rb
@@ -0,0 +1,19 @@
+class User < ApplicationRecord
+  # add validation
+  validates :email,
+  presence: true,
+  uniqueness: { case_sensitive: false }
+  validates :password
+
+  #need to install bcrypt gem, creates a password digest
+  # has_secure_password
+
+  # validates :password,
+  # length: { in: 8..72},
+  # on: :creates
+
+  def self.authenticate(params)
+    User.find_by_email(params[:email]).try(:authenticate, params[:password])
+  end
+
+end
