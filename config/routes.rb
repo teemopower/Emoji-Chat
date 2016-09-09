26fbd974c0f416030f1a3e7aefc6051c37ea9d5e
teemopower <jimmykim925@gmail.com>
2016-09-08 17:54:50 -0700

    fixed bugs

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'home#index'
-  get 'home/need'
-  get 'rooms/show'
+  get 'need' => 'home#need'
+  get 'show'=> 'rooms#show'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
