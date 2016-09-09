7c1d5497b65d3cf67299192bff955c71913423f8
teemopower <jimmykim925@gmail.com>
2016-09-09 10:18:26 -0700

    authentication works

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,19 @@
 Rails.application.routes.draw do
+  
+
+
   root 'home#index'
   get 'need' => 'home#need'
+  
+  ###### CHATROOM ROUTE
   get 'show'=> 'rooms#show'
+
+  ###### AUTHENTICATION ROUTES
+
+  #get 'sessions/new'
+  get "login" => "sessions#new"
+  post "login" => "sessions#create"
+  delete "logout" => "sessions#destroy"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
