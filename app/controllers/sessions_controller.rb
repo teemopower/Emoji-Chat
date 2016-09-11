81a532efe2b39a94b04f098b50aa1ec342827661
teemopower <jimmykim925@gmail.com>
2016-09-10 18:51:06 -0700

    changes to session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -19,6 +19,7 @@ class SessionsController < ApplicationController
   def destroy
     session[:user_id] = nil
     flash[:success] = "User is logged out"
+    reset_session
     redirect_to root_path
   end
 
