ae31193acd8ba1ba772b5aec6712cb570e509b5a
jeremyduanewallace <jeremyduanewallace@gmail.com>
2016-09-13 15:10:19 -0700

    added localhost & heroku cable in production

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -54,7 +54,7 @@ Rails.application.configure do
 
   
   
-  Rails.application.config.action_cable.allowed_request_origins = ['http://localhost:3000', "https://chat-emoji.herokuapp.com/", 'http://chat-emoji.herokuapp.com/' /http:\/\/ruby.*/]
+  config.action_cable.allowed_request_origins = ['http://localhost:3000', "https://chat-emoji.herokuapp.com/", 'http://chat-emoji.herokuapp.com/' /http:\/\/ruby.*/]
 
 
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -39,6 +39,8 @@ Rails.application.configure do
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
+  config.action_cable.allowed_request_origins = ['http://localhost:3000', "https://chat-emoji.herokuapp.com/", 'http://chat-emoji.herokuapp.com/' /http:\/\/ruby.*/]
+
 
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
