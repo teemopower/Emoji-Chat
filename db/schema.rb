0bdb633ef91c97b3ca9f3a53459ae506f68bb460
jeremyduanewallace <jeremyduanewallace@gmail.com>
2016-09-13 14:51:43 -0700

    cable request

--- db/schema.rb
+++ db/schema.rb
@@ -33,12 +33,6 @@ ActiveRecord::Schema.define(version: 20160913005338) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "messages", force: :cascade do |t|
-    t.text     "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "sads", force: :cascade do |t|
     t.text     "content"
     t.datetime "created_at", null: false
