24bca244ab606fd8779417c9d3ce192798f99237
zaturnvalley <zacharycmason@gmail.com>
2016-09-13 16:20:27 -0700

    added user page with update. updated readme file

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,28 +1,34 @@
 class RoomsController < ApplicationController
+  before_action :current_user
   def sad
     @messages = Sad.all
     puts @messages
     puts @messages.class
+
   end
 
   def happy
     @messages = Happy.all
     puts @messages
     puts @messages.class
-    
+
   end
 
   def angry
     @messages = Angry.all
     puts @messages
     puts @messages.class
-    
   end
 
   def calm
     @messages = Calm.all
     puts @messages
     puts @messages.class
-    
+
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :password, :username)
   end
 end
